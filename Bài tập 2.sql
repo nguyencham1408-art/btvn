bd4f9ce8f42d7d119f3c81256e7c9c392bd8cf3e
nguyencham1408-art <nguyencham1408@gmail.com>
2025-11-17 20:24:12 +0700

    ok

---
+++ Bài tập 2.sql
@@ -0,0 +1,30 @@
+CREATE Database quan_ly_kho_sach;
+USE quan_ly_kho_sach;
+
+CREATE TABLE Sach ( 
+MaSach  VARCHAR ( 10 ),
+TenSach  VARCHAR ( 255 ),
+TacGia  VARCHAR ( 255 ),
+TheLoai  VARCHAR ( 100 ),
+GiaBan  INT
+);
+
+INSERT INTO Sach (MaSach, TenSach, TacGia, TheLoai, GiaBan) VALUES 
+( 'SGK01' ,  'Đắc Nhân Tâm' ,  'Dale Carnegie' ,  'Kỹ năng sống' ,  120000 ),
+( 'SVK02' ,  'Nhà Giả Kim' ,  'Paulo Coelho' ,  'Văn học' ,  89000 ),
+( 'STN03' ,  'Lược Sử Người' ,  'Yuval Noah Harari' ,  'Lịch sử' ,  150000 ),
+( 'SKD04' ,  'Tư Duy Nhanh và Lôi' ,  'Daniel Kahneman' ,  'Kinh doanh' ,  99000 );
+
+UPDATE Sach
+SET GiaBan = 180000
+WHERE MaSach = 'SKD04';
+
+UPDATE Sach
+SET TacGia = 'Paulo Coelho'
+WHERE MaSach = 'SVK02';
+
+DELETE FROM Sach
+WHERE MaSach = 'SGK01';
+
+SELECT * FROM Sach;
+
